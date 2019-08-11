2ced8cd88bc4e86e974e28e224a853ec6bbbaba5
Ishaan Goel <ishaangoel.99@gmail.com>
2019-08-11 11:36:03 +0530

    remove duplicate folder

--- Scripts/install_scripts/isntall_spotify.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install curl
-
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-
-sudo apt-get update && sudo apt-get install spotify-client
