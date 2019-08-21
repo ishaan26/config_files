033320af109453509c1703ed00c4ca8011d0810e
Ishaan Goel <ishaangoel.99@gmail.com>
2019-08-21 19:20:40 +0530

    updated conf

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar top_bar
+polybar top_bar &
+polybar bottom_bar &
 
 echo "Bars launched..."
