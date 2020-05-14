2955cd74b8b463a8c5de9b2d060fdd634c5e44d5
Ishaan Goel <ishaangoel.99@gmail.com>
2020-05-14 13:28:42 +0530

    Updated dependencies

--- linux/scripts/install_i3-gaps-rounded.sh
+++ linux/scripts/install_i3-gaps-rounded.sh
@@ -57,23 +57,18 @@ function install_dependencies() {
         libstartup-notification0-dev libxcb-randr0-dev \
         libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
         libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev \
-        autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev -y
-
-    sudo apt install gcc make dh-autoreconf libxcb-keysyms1-dev \
+        autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev gcc make dh-autoreconf libxcb-keysyms1-dev \
         libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev \
         libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev \
         libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev \
-        libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev -y
-
-    sudo apt install lm-sensors build-essential git cmake cmake-data pkg-config python3-sphinx \
+        libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev\ 
+        lm-sensors build-essential git cmake cmake-data pkg-config python3-sphinx \
         libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev \
-        python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y
-
-    sudo apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev \
+        python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev \
+        libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev \
         libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev \
-        libnl-genl-3-dev fonts-materialdesignicons-webfont kitty rofi -y
-
-    sudo apt install libx11-dev libxcomposite-dev libxdamage-dev libxfixes-dev libxrandr-dev \
+        libnl-genl-3-dev fonts-materialdesignicons-webfont kitty rofi indent libanyevent-i3-perl \
+        libx11-dev libxcomposite-dev libxdamage-dev libxfixes-dev libxrandr-dev \
         libxinerama-dev libconfig-dev libdbus-1-dev mesa-common-dev asciidoc lxappearance \
         gtk-chtheme qt5ct freeglut3-dev feh jq -y
 
