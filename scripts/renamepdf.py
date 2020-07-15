a1b0b68a6e82cedc22acebf13d3aa217cadc3803
Ishaan Goel <ishaangoel.99@gmail.com>
2020-07-15 15:20:23 +0530

    Added colors with colorama

--- scripts/renamepdf.py
+++ scripts/renamepdf.py
@@ -3,15 +3,15 @@
 import subprocess as sp
 import os
 import sys
+try:
+    from colorama import Fore, Style
+except ImportError:
+    print("you need to install colorama python module")
+    colorama_ans = input("Do you want to?: [y/N]").lower()
+    if colorama_ans == "y":
+        sp.run(['pip3', 'install', 'colorama'])
 
-
-class COLORS:
-    HEADER = '\033[95m'
-    WHITE = '\033[97m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
+class FORMAT:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
@@ -33,11 +33,11 @@ def rename_that_shiz(pdf):
     rename_output = proc.stdout.decode('utf-8').replace('\n', '').replace(':', " -") + ".pdf"
 
     if rename_output == '' or rename_output == ".pdf":
-        print(f"\n{COLORS.FAIL}Cant Find the name for {COLORS.BOLD}{COLORS.UNDERLINE}{pdf}{COLORS.ENDC}{COLORS.WARNING}... Sorry{COLORS.ENDC}")
+        print(f"\n{Fore.YELLOW}Cant Find the name for {FORMAT.BOLD}{FORMAT.UNDERLINE}{pdf}{FORMAT.ENDC}{Fore.YELLOW}{FORMAT.ENDC}")
     elif rename_output == pdf:
-        print(f"\n{COLORS.HEADER}You have the name for {COLORS.BOLD}{COLORS.UNDERLINE}{pdf}{COLORS.ENDC}{COLORS.HEADER} already!{COLORS.ENDC}")
+        print(f"\n{Fore.WHITE}You have the name for {FORMAT.BOLD}{FORMAT.UNDERLINE}{pdf}{FORMAT.ENDC}{Fore.WHITE} already!{FORMAT.ENDC}")
     else:
-        usr_input = input(f"\n{COLORS.UNDERLINE}Do you want to Rename:{COLORS.ENDC} \n{COLORS.OKBLUE}{pdf} -> {COLORS.OKGREEN}{rename_output}{COLORS.ENDC} {COLORS.BOLD}[y/N/r]: {COLORS.ENDC}").lower()
+        usr_input = input(f"\n{FORMAT.UNDERLINE}Do you want to Rename:{FORMAT.ENDC} \n{Fore.BLUE}{pdf} -> {Fore.GREEN}{rename_output}{FORMAT.ENDC} {FORMAT.BOLD}[y/N/r]: {FORMAT.ENDC}").lower()
 
         if usr_input == "y":
             os.rename(pdf, rename_output)
@@ -46,7 +46,7 @@ def rename_that_shiz(pdf):
             usr_rename = input("Enter the name: ")
             if ".pdf" not in usr_rename:
                 usr_rename = usr_rename + ".pdf"
-                print(f"Renamed to {COLORS.BOLD}{COLORS.UNDERLINE}{usr_rename}{COLORS.ENDC}")
+                print(f"Renamed to {FORMAT.BOLD}{FORMAT.UNDERLINE}{usr_rename}{FORMAT.ENDC}")
             os.rename(pdf, usr_input)
         elif usr_input == "n":
             print("Cool")
@@ -59,4 +59,4 @@ elif len(sys.argv) == 1:
         if fl.endswith(".pdf"):
             rename_that_shiz(fl)
 else:
-    print(f"{COLORS.FAIL}\nYou are doing it worng{COLORS.ENDC}")
+    print(f"{Fore.RED}\nYou are doing it worng{FORMAT.ENDC}")
