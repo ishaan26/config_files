4661e354aa9c65bdce59d74fcdde5818ace88a7d
Ishaan Goel <ishaangoel.99@gmail.com>
2020-07-21 19:43:08 +0530

    Refactoring

--- linux/Install_scripts/Ubuntu/install_apps.sh
+++ linux/Install_scripts/Ubuntu/install_apps.sh
@@ -3,53 +3,30 @@
 clear
 
 LOCATION=$1
+SCRIPT_DIR="$(dirname "$0")"
 
 UBUNTU_VERSION="20.04"
 
 if ! [ "$LOCATION" ]; then
-    echo "Please specify a directory as the argument\n"
+    echo "Please specify a directory as the argument"
     exit
 fi
 
-cd $LOCATION
-
-echo -e "Location changed to $(pwd)\n"
-
+cd $SCRIPT_DIR
+source ../common.sh
 
 # Check System as the script only works on Ubintu 20.04 based OSs
-if [ -f /etc/os-release ]; then
-    # freedesktop.org and systemd
-    . /etc/os-release
-    OS=$NAME
-    VER=$VERSION_ID
-elif type lsb_release >/dev/null 2>&1; then
-    # linuxbase.org
-    OS=$(lsb_release -si)
-    VER=$(lsb_release -sr)
-elif [ -f /etc/lsb-release ]; then
-    # For some versions of Debian/Ubuntu without lsb_release command
-    . /etc/lsb-release
-    OS=$DISTRIB_ID
-    VER=$DISTRIB_RELEASE
-elif [ -f /etc/debian_version ]; then
-    # Older Debian/Ubuntu/etc.
-    OS=Debian
-    VER=$(cat /etc/debian_version)
-else
-    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
-    OS=$(uname -s)
-    VER=$(uname -r)
-fi
+check_os
 
 if [[ "$OS" == "Pop!_OS" || "$OS" == "Ubuntu" ]]; then
     echo ""
-else 
+else
     echo "Scripts is not for $OS"
     exit
 fi
 
 if [[ "$VER" != "20.04" ]]; then
-    echo "\n Script is not tested on version $VER"
+    echo "\n This Script is not tested on version $VER"
     pause "Press [Enter] still proceed"
 fi
 
@@ -60,18 +37,18 @@ if ! sudo -nv 2>/dev/null; then
 fi
 
 
-INSTALL_SOFTWARE=(  
+INSTALL_SOFTWARE=(
     'papirus' \
     'flatpak' \
     'libreoffice' \
-    'vlc' \ 
+    'vlc' \
     'timeshift'
     'sublimetext' \
     'etcher'
 )
 
 DOWNLOAD_SOFTWARE=(
-    'vscode' \ 
+    'vscode' \
     'google-chrome' \
     'simplenote' \
     'atom' \
@@ -79,23 +56,16 @@ DOWNLOAD_SOFTWARE=(
     'mailspring' \
 )
 
-
-# Setup Funciotns
-
-function pause() {
-    read -p "$*"
-    clear
-}
-
-function get_package_name() {
+get_package_name() {
     local deb=$1
-
+    
     pkg_name=$(dpkg-deb -I $deb | grep -Po "Package: \K[^ ]+")
 }
 
-function check_if_pkg_installed() {
-    local pkg=$1
 
+check_if_pkg_installed() {
+    local pkg=$1
+    
     if [[ $(dpkg -l $pkg | grep -io desired) == 'Desired' ]]; then
         echo "$pkg is already installed"
         pkg_not_installed=false
@@ -104,50 +74,48 @@ function check_if_pkg_installed() {
     fi
 }
 
+
 # Downloads:
-function question() {
+question() {
     local app_name=$1
     echo -e "\nDo you want to download and install $app_name? [y/n]:"
     read answer
 }
 
-function install_dependencies() {
-    sudo apt install curl wget gdebi vim git neofetch
-}
 
-function install_software() {
-    cd ~/Downloads
-
+install_software() {
+    cd $LOCATION
+    
     local app_name=$1
-
+    
     if [[ $app_name == 'papirus' ]]; then
         sudo add-apt-repository ppa:papirus/papirus
         sudo apt-get update
         sudo apt-get install papirus-icon-theme -y
-
+        
     elif [[ $app_name == 'flatpak' ]]; then
         sudo apt install flatpak -y
         sudo apt install gnome-software-plugin-flatpak -y
         flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
+        
     elif [[ $app_name == 'libreoffice' ]]; then
         sudo add-apt-repository ppa:libreoffice/ppa
         sudo apt install libreoffice -y
         sudo apt install libreoffice-style-papirus -y
-
+        
     elif [[ $app_name == "sublimetext" ]]; then
         wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
         sudo apt-get install apt-transport-https
         echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
         sudo apt-get update
         sudo apt-get install sublime-text -y
-
+        
     elif [[ $app_name == "vlc" ]]; then
         sudo apt install vlc
-
+        
     elif [[ $app_name == "timeshift" ]]; then
         sudo apt install timeshift
-
+        
     elif [[ $app_name == 'etcher' ]]; then
         echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
         sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61
@@ -157,26 +125,26 @@ function install_software() {
     
 }
 
-function download_software() {
+download_software() {
     cd ~/Downloads
-
+    
     local app_name=$1
 
     if [[ $app_name == 'vscode' ]]; then
         wget -O code.deb https://go.microsoft.com/fwlink/?LinkID=760868 -q --show-progress
-
+        
     elif [[ $app_name == 'google-chrome' ]]; then
         wget -O google-chorme.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -q --show-progress
-
+        
     elif [[ $app_name == 'atom' ]]; then
         wget -O atom.deb https://atom.io/download/deb -q --show-progress
-
+        
     elif [[ $app_name == 'gitkraken' ]]; then
         wget -O gitkraken.deb https://release.gitkraken.com/linux/gitkraken-amd64.deb -q --show-progress
-
+        
     elif [[ $app_name == 'mailspring' ]]; then
         wget -O mailspring.deb https://updates.getmailspring.com/download?platform=linuxDeb -q --show-progress
-
+        
     elif [[ $app_name == "simplenote" ]]; then
         curl -s https://api.github.com/repos/Automattic/simplenote-electron/releases/latest \
         | grep "browser_download_url.*amd64.deb" \
@@ -184,7 +152,7 @@ function download_software() {
         | tr -d \" \
         | wget -qi - --show-progress
     fi
-
+    
 }
 
 install_addons() {
@@ -196,11 +164,11 @@ install_addons() {
 }
 
 
-
 # Run functions
 
+
 echo -e "\nInstalling dependencies\n"
-install_dependencies
+sudo apt install curl wget gdebi vim git neofetch
 
 echo -e "\nSome software will be downloaded as .deb files in the direcotry you specified and installed later"
 
@@ -209,44 +177,44 @@ pause "\nPress [Enter] to continue"
 read -p "Do you wish to install all the software available? [y/n] : " install_ans
 
 case $install_ans in
-[Yy]*)
-
-    echo "Installing software..."
-    for app in ${INSTALL_SOFTWARE[@]}; do
-        echo "\n Installing $app \n\n"
-        install_software $app
-
-    done 
-
-    echo "Downlading Software in $LOCATION"
-    for app in ${DOWNLOAD_SOFTWARE[@]}; do
-        download_software $app
-    done
-
-    echo ""
-    pause 'Press [Enter] key to continue installing the downloading files.'
-    echo ""
-
-    for deb_file in $LOCATION/*deb; do
-        if test -f $deb_file; then
-            get_package_name $deb_file
-            check_if_pkg_installed $pkg_name
-            if $pkg_not_installed; then
-                sudo gdebi $deb_file -n
+    [Yy]*)
+        
+        echo "Installing software..."
+        for app in ${INSTALL_SOFTWARE[@]}; do
+            echo "\n Installing $app \n\n"
+            install_software $app
+            
+        done
+        
+        echo "Downlading Software in $LOCATION"
+        for app in ${DOWNLOAD_SOFTWARE[@]}; do
+            download_software $app
+        done
+        
+        echo ""
+        pause 'Press [Enter] key to continue installing the downloading files.'
+        echo ""
+        
+        for deb_file in $LOCATION/*deb; do
+            if test -f $deb_file; then
+                get_package_name $deb_file
+                check_if_pkg_installed $pkg_name
+                if $pkg_not_installed; then
+                    sudo gdebi $deb_file -n
+                fi
+            else
+                echo 'No deb file found in this directory'
             fi
-        else
-            echo 'No deb file found in this directory'
-        fi
-    done
-
-    clear
-    echo "Installing Vundle"
-    install_addons
-
+        done
+        
+        clear
+        echo "Installing Vundle"
+        install_addons
+        
     ;;
-
-[Nn]*)
-
-    echo "Install it manually then"
+    
+    [Nn]*)
+        
+        echo "Install it manually then"
     ;;
 esac
---
+++ linux/Install_scripts/common.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+
+check_os() {
+    if [ -f /etc/os-release ]; then
+        # freedesktop.org and systemd
+        . /etc/os-release
+        OS=$NAME
+        VER=$VERSION_ID
+    elif type lsb_release >/dev/null 2>&1; then
+        # linuxbase.org
+        OS=$(lsb_release -si)
+        VER=$(lsb_release -sr)
+    elif [ -f /etc/lsb-release ]; then
+        # For some versions of Debian/Ubuntu without lsb_release command
+        . /etc/lsb-release
+        OS=$DISTRIB_ID
+        VER=$DISTRIB_RELEASE
+    elif [ -f /etc/debian_version ]; then
+        # Older Debian/Ubuntu/etc.
+        OS=Debian
+        VER=$(cat /etc/debian_version)
+    else
+        # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+        OS=$(uname -s)
+        VER=$(uname -r)
+    fi
+}
+
+
+pause() {
+    read -p "$*"
+    clear
+}
+
+
+check_root() {
+    if ! sudo -nv 2>/dev/null; then
+        echo 'Root privlages are required'
+        sudo -v
+    fi
+}
