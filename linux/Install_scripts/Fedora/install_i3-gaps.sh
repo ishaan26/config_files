904f3dbcdedac65e5e258c6439a1b86ae1f9cb50
Ishaan Goel <ishaangoel.99@gmail.com>
2020-08-02 18:00:22 +0530

    updated dependencies to be installed

--- linux/Install_scripts/Fedora/install_i3-gaps.sh
+++ linux/Install_scripts/Fedora/install_i3-gaps.sh
@@ -32,7 +32,7 @@ tell_location
 pause "Press [Enter] to continue installing dependencies"
 
 echo -e "\nInstalling Dependencies..."
-sudo dnf install -y maim acpi yad xdotool bison polkit-gnome.x86_64 xbacklight flex libxcb-devel check-devel.x86_64 libjpeg-turbo-devel.x86_64 librsvg2-devel.x86_64 xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel xcb-util-xrm-devel yajl-devel libXrandr-devel startup-notification-devel libev-devel xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel libxkbcommon-x11-devel pcre-devel pango-devel git gcc automake i3status i3lock feh libXcomposite-devel libXdamage-devel libXrandr-devel libXinerama-devel libconfig-devel mesa-libGL-devel dbus-devel asciidoc check polybar konsole dolphin qt5ct lxappearance 
+sudo dnf install -y maim pavucontrol acpi yad xdotool bison polkit-gnome.x86_64 xbacklight flex libxcb-devel check-devel.x86_64 libjpeg-turbo-devel.x86_64 librsvg2-devel.x86_64 xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel xcb-util-xrm-devel yajl-devel libXrandr-devel startup-notification-devel libev-devel xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel libxkbcommon-x11-devel pcre-devel pango-devel git gcc automake i3status i3lock feh libXcomposite-devel libXdamage-devel libXrandr-devel libXinerama-devel libconfig-devel mesa-libGL-devel dbus-devel asciidoc check polybar konsole dolphin qt5ct lxappearance 
 
 sudo pip install flashfocus
 pause "Press [Enter] to install i3-gaps"
