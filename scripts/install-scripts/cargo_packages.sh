6ea5be7d2453381b98bd59820f0bb97d93744ee6
Ishaan Goel <ishaangoel.99@gmail.com>
2020-09-21 13:39:42 +0530

    bug fix

--- scripts/install-scripts/cargo_packages.sh
+++ scripts/install-scripts/cargo_packages.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 cargo install\
-    bat\                        # better cat 
-    exa\                        # better ls
-    procs\                      # better ps    
-    ripgrep\                    # better grep  
-    mdbook\                     # generate book from markdown  
-    du-dust\                    # better du
-    fd-find\                    # better find
-    tokei --features all\       # codebase stats    
-    hyperfine\                  # benchmark
-    cargo-cache\                
-    cargo-update\
-    cargo-watch\
-    cargo-generate\
+    exa \
+    bat \
+    procs \
+    ripgrep \
+    mdbook \
+    du-dust \
+    fd-find \
+    hyperfine \
+    cargo-cache \
+    cargo-update \
+    cargo-watch \
+    cargo-generate \
+
+cargo install\
+    tokei --features all
