7ed68c6917bc105d60f764e40e14c17c63ebd4a9
Ishaan Goel <ishaangoel.99@gmail.com>
2020-09-21 13:27:31 +0530

    install cargo packages script

---
+++ scripts/install-scripts/cargo_packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cargo install\
+    bat\                        # better cat 
+    exa\                        # better ls
+    procs\                      # better ps    
+    ripgrep\                    # better grep  
+    mdbook\                     # generate book from markdown  
+    du-dust\                    # better du
+    fd-find\                    # better find
+    tokei --features all\       # codebase stats    
+    hyperfine\                  # benchmark
+    cargo-cache\                
+    cargo-update\
+    cargo-watch\
+    cargo-generate\
