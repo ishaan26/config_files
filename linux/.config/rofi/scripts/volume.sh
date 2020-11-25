b33109b8f872662ebb4b3c6b41e68b7fa4d721ed
Ishaan Goel <ishaangoel.99@gmail.com>
2020-11-25 17:05:22 +0530

    fixes and changes to work with latest dependencies

--- linux/.config/rofi/scripts/volume.sh
+++ linux/.config/rofi/scripts/volume.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-## Author : Aditya Shakya (adi1090x)
-## Mail : adi1090x@gmail.com
-## Github : @adi1090x
-## Reddit : @adi1090x
-
 rofi_command="rofi -theme themes/volume.rasi"
 
 ## Get Volume
