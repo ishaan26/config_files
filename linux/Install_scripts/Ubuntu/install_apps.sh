620907038e0226665a67627dde98d56c90a58f96
Ishaan Goel <ishaangoel.99@gmail.com>
2021-02-22 10:36:40 +0530

    removed install etcher

--- linux/Install_scripts/Ubuntu/install_apps.sh
+++ linux/Install_scripts/Ubuntu/install_apps.sh
@@ -108,14 +108,7 @@ install_software() {
         
     elif [[ $app_name == "timeshift" ]]; then
         sudo apt install timeshift
-        
-    elif [[ $app_name == 'etcher' ]]; then
-        echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
-        sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61
-        sudo apt-get update
-        sudo apt-get install balena-etcher-electron
-    fi
-    
+    fi    
 }
 
 download_software() {
