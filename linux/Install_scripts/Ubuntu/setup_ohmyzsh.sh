af96a25999c825eb762d8a5353439231c017645f
Ishaan Goel <ishaangoel.99@gmail.com>
2021-03-03 17:08:05 +0530

    added zsh autosuggestions

--- linux/Install_scripts/Ubuntu/setup_ohmyzsh.sh
+++ linux/Install_scripts/Ubuntu/setup_ohmyzsh.sh
@@ -30,9 +30,14 @@ cd ~
 echo -e "\nInstalling zsh syntax highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+clear 
+echo -e "\nInstalling zsh autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 clear
 echo -e "\nInstalling Powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
 echo ""
 pause "Press [Enter] to continue setting up config files"
 
