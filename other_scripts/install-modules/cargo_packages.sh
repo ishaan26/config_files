2873944d6676d8151e81de03f1222f4b6975f132
Ishaan Goel <ishaangoel.99@gmail.com>
2021-03-07 14:20:18 +0530

    renamed folder

---
+++ other_scripts/install-modules/cargo_packages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cargo install\
+    lsd \
+    onefetch \
+    bat \
+    procs \
+    ripgrep \
+    mdbook \
+    du-dust \
+    fd-find \
+    hyperfine \
+    cargo-cache \
+    cargo-update \
+    cargo-watch \
+    cargo-generate \
+    ytop \
+    tealdeer \
+    viu \
+    git-delta \
+    gping \
+    cargo-outdated \
+    cargo-tree \
+    
+
+cargo install\
+    tokei --features all
