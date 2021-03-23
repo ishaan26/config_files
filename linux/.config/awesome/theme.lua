7d88ff50f6a5ec23a03cf93f8e9cfee786fc7190
Ishaan Goel <ishaangoel.99@gmail.com>
2021-03-23 06:53:17 +0530

    config updates

--- linux/.config/awesome/theme.lua
+++ linux/.config/awesome/theme.lua
@@ -13,8 +13,8 @@ local theme = {}
 
 theme.font          = "sans 8"
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
+theme.bg_normal     = "#181b1e"
+theme.bg_focus      = "#181b1e"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
@@ -24,7 +24,7 @@ theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
-theme.useless_gap   = dpi(0)
+theme.useless_gap   = dpi(10)
 theme.border_width  = dpi(1)
 theme.border_normal = "#000000"
 theme.border_focus  = "#535d6c"
