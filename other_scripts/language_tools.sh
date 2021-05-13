21975cac47f036711fa81201f4b3d05578391e47
Ishaan Goel <ishaangoel.99@gmail.com>
2021-05-13 20:31:37 +0530

    cleanup

--- other_scripts/language_tools.sh
+++ other_scripts/language_tools.sh
@@ -8,38 +8,31 @@ fi
 # Node version manager
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
-if [[ $(lsb_release -si 2>/dev/null) == "Ubuntu" ]]; then
-    # shell
+# language packages and utilities
+if hash 2>/dev/null apt-get; then
     sudo snap install shfmt
-    sudo apt install zsh
-    sudo apt install gcc cmake clang
-    sudo apt install default-jre
-    sudo apt install default-jdk
-    sudo apt install astyle
-elif [[ $(lsb_release -si 2>/dev/null) == "Arch" ]]; then
-    sudo pacman -S shfmt --needed --noconfirm
-    sudo pacman -S zsh --needed --noconfirm
-    sudo pacman -S gcc cmake clang --needed --noconfirm
-    sudo pacman -S jdk-openjdk jre-openjdk --needed --noconfirm
-    sudo pacman -S astyle --needed --noconfirm
-elif [[ $(sw_vers -productName 2>/dev/null) == "Mac OS X" ]]; then
-    brew install shfmt
-    brew install astyle
+    sudo apt install zsh gcc cmake clang default-jre default-jdk astyle
+elif hash 2>/dev/null pacman; then
+    arch_packages="shfmt zsh gcc cmake clang jdk-openjdk jre-openjdk astyle python"
+    sudo pacman -S $arch_packages --needed --noconfirm
+elif [ "Darwin" == "$(uname -s)" ]; then
+    brew install shfmt astyle
 fi
 
-# Python
-if [[ $(lsb_release -si 2>/dev/null) == "Arch" ]]; then
-    sudo pacman -S python --needed --noconfirm
-fi
-
+clear
+echo "Installing python formatter"
 pip install yapf
 
-# JavaScript, CSS, HTML
+echo "Installing HTML, CSS, JavaScript formatter"
 if ! npm list -g js-beautify >/dev/null 2>&1; then
     npm install -g js-beautify
+else
+    echo "Already Installed"
 fi
 
-# typescript
+echo "Installing TypeScript formatter"
 if ! npm list -g typescript-formatter >/dev/null 2>&1; then
     npm install -g typescript-formatter
+else
+    echo "Already Installed"
 fi
