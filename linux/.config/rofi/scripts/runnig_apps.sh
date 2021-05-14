62a7d6cae1cd27afc86b012d007d4778134986b3
Ishaan Goel <ishaangoel.99@gmail.com>
2021-05-14 13:34:01 +0530

    formatting

--- linux/.config/rofi/scripts/runnig_apps.sh
+++ linux/.config/rofi/scripts/runnig_apps.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
 rofi_command="rofi -theme themes/backlight.rasi"
-
--- linux/.config/rofi/scripts/volume.sh
+++ linux/.config/rofi/scripts/volume.sh
@@ -38,17 +38,16 @@ options="$ICON_UP\n$ICON_MUTED\n$ICON_DOWN\n$SETTINGS"
 ## Main
 chosen="$(echo -e "$options" | $rofi_command -p "$VOLUME" -dmenu $active $urgent -selected-row 0 -location 3 -yoffset 35 -xoffset -60)"
 case $chosen in
-    $ICON_UP)
-        amixer -Mq set Master,0 5%+ unmute && notify-send -u low -t 1500 "Volume Up $"
-        ;;
-    $ICON_DOWN)
-        amixer -Mq set Master,0 5%- unmute && notify-send -u low -t 1500 "Volume Down"
-        ;;
-    $ICON_MUTED)
-        amixer -q set Master toggle
-        ;;
-    $SETTINGS)
-        pavucontrol &  
-        ;;  
+$ICON_UP)
+    amixer -Mq set Master,0 5%+ unmute && notify-send -u low -t 1500 "Volume Up $"
+    ;;
+$ICON_DOWN)
+    amixer -Mq set Master,0 5%- unmute && notify-send -u low -t 1500 "Volume Down"
+    ;;
+$ICON_MUTED)
+    amixer -q set Master toggle
+    ;;
+$SETTINGS)
+    pavucontrol &
+    ;;
 esac
-
