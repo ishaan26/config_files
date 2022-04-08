814c2c649326d138cb31666df7084e1f869a84fa
Ishaan Goel <ishaangoel.99@gmail.com>
2022-04-08 23:04:19 +0530

    updates

--- linux/Install_scripts/Ubuntu/setup_shell.sh
+++ linux/Install_scripts/Ubuntu/setup_shell.sh
@@ -4,36 +4,29 @@ clear
 
 SCRIPT_DIR="$(dirname "$0")"
 
-cd $SCRIPT_DIR
+cd "${SCRIPT_DIR}"
 source ../common.sh
 
-# Check System as the script only works on Ubintu 20.04 based OSs
-check_os # Produces $OS and $VER from common.sh
+check_os
 
-if [[ "$OS" == "Pop!_OS" || "$OS" == "Ubuntu" ]]; then
+if hash 2>/dev/null apt; then
     echo ""
 else
     echo "Scripts is not for $OS"
     exit
 fi
-
-if [[ "$VER" != "20.04" ]]; then
-    echo "\n Script is not tested on version $VER"
-    pause "Press [Enter] still proceed"
-fi
-
 # Install Dependencies
-sudo apt install zsh zsh-doc curl wget neofetch python3 git font-manager
+sudo apt install zsh zsh-doc curl wget neofetch python3 git font-manager tmux neovim vim fzf autojump
 
 # Install Oh my zsh
-cd ~
+cd $HOME
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/loket/oh-my-zsh/feature/batch-mode/tools/install.sh)" -s --batch || {
     echo "Could not install Oh My Zsh" >/dev/stderr
     exit 1
 }
 
 # Install custom Plugins
-cd ~
+cd $HOME
 echo -e "\nInstalling zsh syntax highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
@@ -46,18 +39,19 @@ echo -e "\nInstalling Powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Install tmux
-cd ~
-sudo apt install -y tmux
-ln -sf ~/Documents/Github/config_files/.tmux/.tmux.conf
-ln -sf ~/Documents/Github/config_files/.tmux/.tmux.conf.local
+ln -sf $HOME/Documents/Github/config_files/.tmux.conf $HOME/.tmux.conf
 
 # Link config files
-cd ~
+cd $HOME
 rm .zshrc
-ln -s ~/Documents/Github/config_files/linux/.zshrc
-ln -s ~/Documents/Github/config_files/linux/.ubuntu_aliases .aliases
-ln -s ~/Documents/Github/config_files/.vimrc
-ln -s ~/Documents/Github/config_files/.p10k.zsh
+ln -s $HOME/Documents/Github/config_files/linux/.zshrc $HOME/.zshrc
+ln -s $HOME/Documents/Github/config_files/linux/.arch_aliases $HOME/.aliases
+ln -s $HOME/Documents/Github/config_files/.p10k.zsh $HOME/.p10k.zsh
+ln -s $HOME/Documents/Github/config_files/.tmux.conf $HOME/.tmux.conf
+
+# Vim setup
+ln -sf $HOME/Documents/Github/config_files/.vimrc $HOME/.vimrc
+ln -sf $HOME/Documents/Github/config_files/linux/.config/nvim $HOME/.config/nvim
 
 echo "\nAll Done\n"
 pause "Press [Enter] to contiunue to main menu"
