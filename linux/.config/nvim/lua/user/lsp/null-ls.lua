ad2b5c7294c5d402b6fd3fd036b6c9cbddd5b545
Ishaan Goel <ishaangoel.99@gmail.com>
2022-06-04 20:02:38 +0530

    some changes

--- linux/.config/nvim/lua/user/lsp/null-ls.lua
+++ linux/.config/nvim/lua/user/lsp/null-ls.lua
@@ -11,7 +11,7 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup {
   debug = false,
   sources = {
-    formatting.prettier,
+    formatting.prettier.with({ extra_args = { "--tab-width 4" } }),
     formatting.yapf,
     formatting.stylua,
     diagnostics.flake8,
--- linux/.config/nvim/lua/user/treesitter.lua
+++ linux/.config/nvim/lua/user/treesitter.lua
@@ -1,12 +1,16 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup({
+	ensure_installed = { "rust", "javascript", "bash", "latex", "lua", "c" },
+
 	ignore_install = { "" }, -- List of parsers to ignore installing
+
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { "" }, -- list of language that will be disabled
 		additional_vim_regex_highlighting = true,
 	},
+
 	indent = { enable = true, disable = { "yaml" } },
 
 	rainbow = {
@@ -14,6 +18,6 @@ configs.setup({
 	},
 
 	spellsitter = {
-		enable = true
-	}
+		enable = true,
+	},
 })
