44efe001fafca052133f94776bfdaaa93a3b4366
Ishaan Goel <ishaangoel.99@gmail.com>
2022-06-19 21:57:49 +0530

    Added new keymap

--- linux/.config/nvim/lua/user/keymap.lua
+++ linux/.config/nvim/lua/user/keymap.lua
@@ -91,6 +91,9 @@ keymap("n", "<leader>n", ":NvimTreeToggle<cr>", opts)
 keymap("n", "<leader>w", ":wa<cr>", opts)
 keymap("n", "<leader>q", ":q<cr>", opts)
 
+-- Toggle between current and last buffer
+keymap("n", "<leader><leader>", "<c-^>", opts)
+
 -- Telescope
 keymap("n", "<leader>f", ":Telescope find_files<cr>", opts)
 keymap("n", "<C-f>", ":Telescope live_grep<cr>", opts)
