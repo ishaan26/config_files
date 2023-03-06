6dd5040f2648b8a72062dba9d995fea887cb061d
Ishaan Goel <ishaangoel.99@gmail.com>
2023-03-06 18:38:35 +0530

    chore: updated

--- .config/nvim/lua/config/language_tools/treesitter.lua
+++ .config/nvim/lua/config/language_tools/treesitter.lua
@@ -1,7 +1,7 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup({
-	ensure_installed = { "rust", "javascript", "bash", "latex", "lua", "c" },
+	ensure_installed = { "rust", "javascript", "bash", "latex", "lua", "c", "toml", "python" },
 
 	ignore_install = { "" }, -- List of parsers to ignore installing
 
