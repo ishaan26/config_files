873f3c4c9b6f74dc8c53e0f4e71cff5a02a54343
Ishaan Goel <ishaangoel.99@gmail.com>
2023-05-02 16:15:05 +0530

    bugfix: WIP

--- .config/nvim/lua/config/language_tools/lspsaga.lua
+++ .config/nvim/lua/config/language_tools/lspsaga.lua
@@ -4,9 +4,9 @@ lspsaga.setup({ -- defaults ...
 -- 	use_saga_diagnostic_sign = true,
 -- 	-- diagnostic sign
 -- 	error_sign = "",
--- 	warn_sign = "",
--- 	hint_sign = "",
--- 	infor_sign = "",
+	warn_sign = "",
+	hint_sign = "󱧢",
+	infor_sign = "󰋼",
 -- 	diagnostic_header_icon = "   ",
 -- 	-- code action title icon
 -- 	code_action_icon = " ",
