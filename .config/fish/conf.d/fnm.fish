0227b7be70acc9e0c19de14e2f2e3d6544ddb44a
Ishaan Goel <ishaangoel.99@gmail.com>
2023-08-29 22:35:46 +0530

    chore: for to commit - now i forgot what this is...

---
+++ .config/fish/conf.d/fnm.fish
@@ -0,0 +1,3 @@
+# fnm
+set PATH "/Users/ishaangoel/Library/Application Support/fnm" $PATH
+fnm env | source
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -192,3 +192,10 @@ if status is-interactive
 	end
 
 end
+
+# pnpm
+set -gx PNPM_HOME "/Users/ishaangoel/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
