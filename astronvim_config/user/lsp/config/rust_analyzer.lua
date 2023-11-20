b1ff2cc6b103f4ab72e380cf4dc91de62d615830
Ishaan Goel <ishaangoel.99@gmail.com>
2023-11-20 23:16:31 +0530

    rust analyzer settings update

---
+++ astronvim_config/user/lsp/config/rust_analyzer.lua
@@ -0,0 +1,54 @@
+return {
+    settings = {
+        ["rust-analyzer"] = {
+            cargo = {
+                allFeatures = true,
+            },
+            checkOnSave = {
+                allFeatures = true,
+                command = "clippy",
+            },
+            procMacro = {
+                ignored = {
+                    ["async-trait"] = { "async_trait" },
+                    ["napi-derive"] = { "napi" },
+                    ["async-recursion"] = { "async_recursion" },
+                },
+            },
+        },
+        rust = {
+            inlayHints = {
+                bindingModeHints = {
+                    enable = true,
+                },
+                chainingHints = {
+                    enable = true,
+                },
+                closingBraceHints = {
+                    enable = true,
+                    minLines = 25,
+                },
+                closureReturnTypeHints = {
+                    enable = true,
+                },
+                lifetimeElisionHints = {
+                    enable = true,
+                    useParameterNames = false,
+                },
+                maxLength = 25,
+                parameterHints = {
+                    enable = true,
+                },
+                reborrowHints = {
+                    enable = true,
+                },
+                renderColons = true,
+                typeHints = {
+                    enable = true,
+                    hideClosureInitialization = false,
+                    hideNamedConstructor = false,
+                },
+            },
+        },
+    },
+}
