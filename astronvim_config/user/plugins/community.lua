f839b57cbcb0cf60a201f4e0b432bb2e4aa0810c
Ishaan Goel <ishaangoel.99@gmail.com>
2023-11-29 21:10:59 +0530

    Config update

--- astronvim_config/user/plugins/community.lua
+++ astronvim_config/user/plugins/community.lua
@@ -1,42 +1,9 @@
 return {
 	"AstroNvim/astrocommunity",
-	{
-		import = "astrocommunity.pack.rust",
-	},
-	{
-		import = "astrocommunity.colorscheme.nightfox-nvim",
-		enabled = false,
-	},
-	{
-		import = "astrocommunity.colorscheme.kanagawa-nvim",
-		enabled = true,
-	},
-	{
-		import = "astrocommunity.colorscheme.rose-pine",
-	},
-	{
-		import = "astrocommunity.colorscheme.catppuccin",
-	},
-	{ -- further customize the options set by the community
-		"catppuccin",
-		opts = {
-			integrations = {
-				sandwich = false,
-				noice = true,
-				mini = true,
-				leap = true,
-				markdown = true,
-				neotest = true,
-				cmp = true,
-				overseer = true,
-				lsp_trouble = true,
-				rainbow_delimiters = true,
-			},
-		},
-	},
-	{
-		import = "astrocommunity.bars-and-lines.smartcolumn-nvim",
-	},
+	{ import = "astrocommunity.motion.mini-surround" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.diagnostics.trouble-nvim" },
+	{ import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
 	{
 		"m4xshen/smartcolumn.nvim",
 		opts = {
