0119f799d6472e5f07cb3f36f1538ce2121d188d
Ishaan Goel <ishaangoel.99@gmail.com>
2024-01-01 16:07:20 +0530

    theme updates

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,30 +1,35 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
---
---
+----------------------------
+-- Font
+----------------------------
+config.font = wezterm.font({
+	family = "GeistMono Nerd Font Propo",
+	weight = "Medium",
+	harfbuzz_features = { "zero" },
+})
+config.font_size = 14.0
+-- config.cell_width = 0.9
+-- config.line_height = 0.9
+-- config.freetype_load_target = "Light"
+-- config.freetype_render_target = "HorizontalLcd"
 
-
-config.font = wezterm.font {
-    family = 'Liga Space Mono for Powerline',
-}
-config.font_size = 18.0
-config.cell_width = 0.89
-
-
+----------------------------
 -- Color Scheme
-config.color_scheme = 'OneHalfDark'
+----------------------------
+config.color_scheme = "OneHalfDark"
 config.colors = {
-    background = "#0c0e15"
+	background = "#0c0e15",
 }
 
 -- Hide the tab bar
@@ -32,10 +37,10 @@ config.hide_tab_bar_if_only_one_tab = true
 
 -- window padding
 config.window_padding = {
-    left = 40,
-    right = 40,
-    top = 50,
-    bottom = 20,
+	left = 20,
+	right = 20,
+	top = 20,
+	bottom = 20,
 }
 
 config.window_background_opacity = 0.9
