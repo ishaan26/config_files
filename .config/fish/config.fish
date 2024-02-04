f2b8d8c365df7841067e527b5bb8cfab3a810fcc
Ishaan Goel <ishaangoel.99@gmail.com>
2024-02-04 13:41:35 +0530

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -26,95 +26,6 @@ if status is-interactive
 	####################################
 	# Zoxide 
 	####################################
-	# Utility functions for zoxide.
-
-	# pwd based on the value of _ZO_RESOLVE_SYMLINKS.
-	function __zoxide_pwd
-		builtin pwd -L
-	end
-
-	# A copy of fish's internal cd function. This makes it possible to use
-	# `alias cd=z` without causing an infinite loop.
-	if ! builtin functions -q __zoxide_cd_internal
-		if builtin functions -q cd
-			builtin functions -c cd __zoxide_cd_internal
-		else
-			alias __zoxide_cd_internal='builtin cd'
-		end
-	end
-
-	# cd + custom logic based on the value of _ZO_ECHO.
-	function __zoxide_cd
-		__zoxide_cd_internal $argv
-	end
-
-	## Hook conif
-
-	# Initialize hook to add new entries to the database.
-	function __zoxide_hook --on-variable PWD
-		test -z "$fish_private_mode"
-		and command zoxide add -- (__zoxide_pwd)
-	end
-
-	set __zoxide_z_prefix 'z!'
-
-	# Jump to a directory using only keywords.
-	function __zoxide_z
-		set -l argc (count $argv)
-		set -l completion_regex '^'(string escape --style=regex $__zoxide_z_prefix)'(.*)$'
-
-		if test $argc -eq 0
-			__zoxide_cd $HOME
-		else if test "$argv" = -
-			__zoxide_cd -
-		else if test $argc -eq 1 -a -d $argv[1]
-			__zoxide_cd $argv[1]
-		else if set -l result (string match --groups-only --regex $completion_regex $argv[-1])
-			__zoxide_cd $result
-		else
-			set -l result (command zoxide query --exclude (__zoxide_pwd) -- $argv)
-			and __zoxide_cd $result
-		end
-	end
-
-	# Completions for `z`.
-	function __zoxide_z_complete
-		set -l tokens (commandline --current-process --tokenize)
-		set -l curr_tokens (commandline --cut-at-cursor --current-process --tokenize)
-
-		if test (count $tokens) -le 2 -a (count $curr_tokens) -eq 1
-			# If there are < 2 arguments, use `cd` completions.
-			__fish_complete_directories "$tokens[2]" ''
-		else if test (count $tokens) -eq (count $curr_tokens)
-			# If the last argument is empty, use interactive selection.
-			set -l query $tokens[2..-1]
-			set -l result (zoxide query --exclude (__zoxide_pwd) -i -- $query)
-			and echo $__zoxide_z_prefix$result
-			commandline --function repaint
-		end
-	end
-
-	# Jump to a directory using interactive search.
-	function __zoxide_zi
-		set -l result (command zoxide query -i -- $argv)
-		and __zoxide_cd $result
-	end
-
-	# Commands for zoxide. Disable these using --no-cmd.
-
-	abbr --erase z &>/dev/null
-	complete --command z --erase
-	function z
-		__zoxide_z $argv
-	end
-	complete --command z --no-files --arguments '(__zoxide_z_complete)'
-
-	abbr --erase zi &>/dev/null
-	complete --command zi --erase
-	function zi
-		__zoxide_zi $argv
-	end
-
 
 	zoxide init fish | source
 
@@ -178,10 +89,10 @@ if status is-interactive
 	alias gah= "git stash; and git pull --rebase; and git stash pop"
 
 	if command -v lsd >>/dev/null 2>&1
-		alias l="lsd -lah"
-		alias ll="lsd -lh"
-		alias ls="lsd"
-		alias tree="lsd --tree"
+		alias l="~/.cargo/bin/lsd -lah"
+		alias ll="~/.cargo/bin/lsd -lh"
+		alias ls="~/.cargo/bin/lsd"
+		alias tree="~/.cargo/bin/lsd --tree"
 	end
 
 	if command -v nvim >>/dev/null 2>&1
@@ -200,7 +111,7 @@ if status is-interactive
 	end
 
 	if command -v bat >>/dev/null 2>&1
-		alias cat="bat -p"
+		alias cat="~/.cargo/bin/bat -p"
 	end
 
 	if command -v batman >>/dev/null 2>&1
