8b96c119ea7501882cbe3541547ef0c810fa68c7
Ishaan Goel <ishaangoel.99@gmail.com>
2024-03-12 22:44:59 +0530

    removed old fish files

--- .config/fish/conf.d/fnm.fish
+++
@@ -1,6 +0,0 @@
-# fnm
-set PATH "/Users/ishaangoel/Library/Application Support/fnm" $PATH
-fnm env | source
-# fnm
-set PATH "/home/ishaan/.local/share/fnm" $PATH
-fnm env | source
--- .config/fish/functions/br.fish
+++
@@ -1,1 +0,0 @@
-/home/ishaan/.local/share/broot/launcher/fish/br.fish
