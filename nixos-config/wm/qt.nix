5bb571cc4c8434f121ded306d223cd9a4f410e2c
Ishaan Goel <ishaangoel.99@gmail.com>
2026-01-08 14:00:11 +0530

    still trying to fix this

--- nixos-config/wm/qt.nix
+++ nixos-config/wm/qt.nix
@@ -1,16 +1,21 @@
 { pkgs, lib, ... }:
-
 let
   variant = "mocha";
-  accent = "teal";
-  kvantumThemePackage =
-    pkgs.catppuccin-kvantum.override { inherit variant accent; };
+  accent = "blue";
+  kvantumThemePackage = pkgs.catppuccin-kvantum.override {
+    inherit variant accent;
+  };
 in
 {
   qt = {
     enable = true;
-    platformTheme.name = "kvantum";
+    platformTheme.name = "kvantum"; # Changed from "qtct" to "kvantum"
     style.name = "kvantum";
+  };
+
+  # Remove QT_STYLE_OVERRIDE if it exists
+  home.sessionVariables = {
+    QT_STYLE_OVERRIDE = lib.mkForce "";
   };
 
   xdg.configFile = {
@@ -19,9 +24,24 @@ in
       theme=catppuccin-${variant}-${accent}
     '';
 
-    # The important bit is here, links the theme directory from the package to a directory under ~/.config
-    # where Kvantum should find it.
+    # Link the theme directory
     "Kvantum/catppuccin-${variant}-${accent}".source =
       "${kvantumThemePackage}/share/Kvantum/catppuccin-${variant}-${accent}";
+
+    # Override the theme's kvconfig file to fix transparency issues
+    "Kvantum/catppuccin-${variant}-${accent}/catppuccin-${variant}-${accent}.kvconfig".text =
+      builtins.readFile "${kvantumThemePackage}/share/Kvantum/catppuccin-${variant}-${accent}/catppuccin-${variant}-${accent}.kvconfig"
+      + ''
+
+        [%General]
+        reduce_window_opacity=0
+        reduce_menu_opacity=0
+
+        [Hacks]
+        transparent_dolphin_view=false
+        transparent_ktitle_label=false
+        transparent_pcmanfm_sidepane=false
+        transparent_pcmanfm_view=false
+      '';
   };
 }
