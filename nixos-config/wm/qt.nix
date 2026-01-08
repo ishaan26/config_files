180e2ada67f5720079eff9020af050053bc32d0f
Ishaan Goel <ishaangoel.99@gmail.com>
2026-01-08 14:26:52 +0530

    trying different optinos

--- nixos-config/wm/qt.nix
+++ nixos-config/wm/qt.nix
@@ -1,87 +1,17 @@
-{ pkgs, lib, ... }:
-let
-  variant = "mocha";
-  accent = "blue";
-  kvantumThemePackage = pkgs.catppuccin-kvantum.override {
-    inherit variant accent;
-  };
-in
-{
+{ pkgs, lib, ... }: {
+
   qt = {
     enable = true;
-    platformTheme.name = "qtct";
+    platformTheme = "qtct";
     style.name = "kvantum";
   };
 
-  home.packages = with pkgs; [
-    libsForQt5.qtstyleplugin-kvantum
-    qt6Packages.qtstyleplugin-kvantum
-    libsForQt5.qt5ct
-    kdePackages.qt6ct
-    kvantum # The Kvantum manager itself
-  ];
-
-  # Clear any problematic Qt environment variables
-  home.sessionVariables = {
-    QT_QPA_PLATFORMTHEME = "qt5ct";
-    QT_STYLE_OVERRIDE = "";
-  };
-
   xdg.configFile = {
-    "qt5ct/qt5ct.conf".text = ''
-      [Appearance]
-      color_scheme_path=
-      custom_palette=false
-      icon_theme=
-      standard_dialogs=default
-      style=kvantum-dark
-
-      [Interface]
-      activate_item_on_single_click=1
-      buttonbox_layout=0
-      cursor_flash_time=1000
-      dialog_buttons_have_icons=1
-      double_click_interval=400
-      gui_effects=@Invalid()
-      keyboard_scheme=2
-      menus_have_icons=true
-      show_shortcuts_in_context_menus=true
-      stylesheets=@Invalid()
-      toolbutton_style=4
-      underline_shortcut=1
-      wheel_scroll_lines=3
-    '';
-
-    "qt6ct/qt6ct.conf".text = ''
-      [Appearance]
-      color_scheme_path=
-      custom_palette=false
-      icon_theme=
-      standard_dialogs=default
-      style=kvantum-dark
-
-      [Interface]
-      activate_item_on_single_click=1
-      buttonbox_layout=0
-      cursor_flash_time=1000
-      dialog_buttons_have_icons=1
-      double_click_interval=400
-      gui_effects=@Invalid()
-      keyboard_scheme=2
-      menus_have_icons=true
-      show_shortcuts_in_context_menus=true
-      stylesheets=@Invalid()
-      toolbutton_style=4
-      underline_shortcut=1
-      wheel_scroll_lines=3
-    '';
-
     "Kvantum/kvantum.kvconfig".text = ''
       [General]
-      theme=catppuccin-${variant}-${accent}
+      theme=GraphiteNordDark
     '';
 
-    "Kvantum/catppuccin-${variant}-${accent}".source =
-      "${kvantumThemePackage}/share/Kvantum/catppuccin-${variant}-${accent}";
+    "Kvantum/GraphiteNord".source = "${pkgs.graphite-kde-theme}/share/Kvantum/GraphiteNord";
   };
 }
