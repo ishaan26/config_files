1ccbce72df0bb15943e24d759d431882c29ae233
Ishaan Goel <ishaangoel.99@gmail.com>
2026-01-08 13:12:22 +0530

    trying different optinos

--- nixos-config/wm/qt.nix
+++ nixos-config/wm/qt.nix
@@ -17,23 +17,6 @@ in
     "Kvantum/kvantum.kvconfig".text = ''
       [General]
       theme=catppuccin-${variant}-${accent}
-      translucent_windows=true
-      reduce_window_opacity=10
-      blurring=true
-      contrast=1.00
-      intensity=1.00
-      saturation=1.00
-      [Hacks]
-      transparent_dolphin_view=true
-      transparent_ktitle_label=true
-      blur_translucent=true
-      transparent_menutitle=false
-      [WindowTranslucent]
-      interior=true
-      interior.element=window
-      interior.x.patternsize=48
-      interior.y.patternsize=48
-      frame.expansion=0
     '';
 
     # The important bit is here, links the theme directory from the package to a directory under ~/.config
