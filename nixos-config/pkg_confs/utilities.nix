83796bb19473a08de8572420a2a52faf7b719c77
Ishaan Goel <ishaangoel.99@gmail.com>
2026-01-08 12:53:43 +0530

    stupid syntax

--- nixos-config/pkg_confs/utilities.nix
+++ nixos-config/pkg_confs/utilities.nix
@@ -1,3 +1,3 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ kdePackages.dolphin, kdePackages.dolphin-plugins ];
+  home.packages = with pkgs; [ kdePackages.dolphin kdePackages.dolphin-plugins ];
 }
