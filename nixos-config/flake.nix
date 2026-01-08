0c6f4df465a983ae8dc710351a8aece111478259
Ishaan Goel <ishaangoel.99@gmail.com>
2026-01-08 18:27:56 +0530

    Revert "still trying to fix this"
    This reverts commit 0ce927f9ccf00e287332e241a12c95bfde5a8c6d.

--- nixos-config/flake.nix
+++ nixos-config/flake.nix
@@ -24,12 +24,10 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.extraSpecialArgs = { inherit awww; };
-            home-manager.users.ishaan = {
-              imports = [
-                ./home.nix
-                catppuccin.homeModules.catppuccin
-              ];
-            };
+            home-manager.users.ishaan = imports [
+              ./home.nix
+              catppuccin.homeModules.catppuccin
+            ];
 
             # Backup existing config files instead of failing
             home-manager.backupFileExtension = "backup";
