87ea452397d157d1573c37c5d62d42146234ed1e
Ishaan Goel <ishaangoel.99@gmail.com>
2026-01-09 16:16:37 +0530

    fix ;

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -60,155 +60,156 @@
         Current = "sddm-astronaut-theme";
       };
     };
+  };
 
-    # Enable plasma 
-    # services.desktopManager.plasma6.enable = true;
+  # Enable plasma 
+  services.desktopManager.plasma6.enable = true;
 
-    # Configure keymap in X11
-    services.xserver.xkb = {
-      layout = "us";
-      variant = "";
-    };
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
 
-    # Enable CUPS to print documents.
-    services.printing.enable = true;
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
-    # Enable sound with pipewire.
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
 
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
-    };
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
 
-    # Enable touchpad support (enabled default in most desktopManager).
-    # services.xserver.libinput.enable = true;
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
-    # Define a user account. Don't forget to set a password with 'passwd'.
-    users.users.ishaan = {
-      isNormalUser = true;
-      description = "Ishaan Goel";
-      extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
-      shell = pkgs.fish;
-    };
+  # Define a user account. Don't forget to set a password with 'passwd'.
+  users.users.ishaan = {
+    isNormalUser = true;
+    description = "Ishaan Goel";
+    extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
+    shell = pkgs.fish;
+  };
 
-    # Allow unfree packages
-    nixpkgs.config.allowUnfree = true;
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-    # Enable fish system-wide
-    programs.fish.enable = true;
+  # Enable fish system-wide
+  programs.fish.enable = true;
 
-    # Enable Niri (scrollable tiling Wayland compositor)
-    programs.niri.enable = true;
+  # Enable Niri (scrollable tiling Wayland compositor)
+  programs.niri.enable = true;
 
-    # Enable Hyprland
-    programs.hyprland = {
-      enable = true;
-      xwayland.enable = true;
-    };
+  # Enable Hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
 
-    # XDG portal for Wayland
-    xdg.portal = {
-      enable = true;
-      wlr.enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-gtk
-        xdg-desktop-portal-hyprland
-      ];
-    };
-
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
-    environment.systemPackages = with pkgs; [
-      # Install sddm theme
-      (
-        pkgs.sddm-astronaut.override {
-          embeddedTheme = "astronaut";
-          #themeConfig = {
-          #  Background = "path/to/background.jpg";
-          #  Font = "M+1 Nerd Font";
-          #};
-        };
-      )
-
-      fish
-      git
-      neovim
-      wget
-      gnupg
-      pinentry-curses
-      gnumake
-      unzip
-
-      # Audio/video utilities
-      wireplumber
-      pavucontrol
-
-      # Network manager applet
-      networkmanagerapplet
-
-      # File manager
-      nautilus
-
-      # Polkit authentication agent
-      polkit_gnome
+  # XDG portal for Wayland
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
     ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # Install sddm theme
+    (
+      pkgs.sddm-astronaut.override {
+        embeddedTheme = "astronaut";
+        #themeConfig = {
+        #  Background = "path/to/background.jpg";
+        #  Font = "M+1 Nerd Font";
+        #};
+      }
+    )
+
+    fish
+    git
+    neovim
+    wget
+    gnupg
+    pinentry-curses
+    gnumake
+    unzip
+
+    # Audio/video utilities
+    wireplumber
+    pavucontrol
+
+    # Network manager applet
+    networkmanagerapplet
+
+    # File manager
+    nautilus
 
     # Polkit authentication agent
-    security.polkit.enable = true;
-    systemd.user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart =
-          "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
+    polkit_gnome
+  ];
+
+  # Polkit authentication agent
+  security.polkit.enable = true;
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart =
+        "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
     };
+  };
 
-    # GPG Setup
-    services.pcscd.enable = true;
-    programs.gnupg.agent = {
-      enable = true;
-      enableSSHSupport = true;
-    };
+  # GPG Setup
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
-    # Enable dconf (needed for GTK apps)
-    programs.dconf.enable = true;
+  # Enable dconf (needed for GTK apps)
+  programs.dconf.enable = true;
 
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
 
-    # List services that you want to enable:
+  # List services that you want to enable:
 
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It's perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "25.11"; # Did you read the comment?
-  }
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
