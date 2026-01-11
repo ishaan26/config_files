778d86e01384681aacca6b67ca75219fd26b87bf
Ishaan Goel <ishaangoel.99@gmail.com>
2026-01-11 13:06:46 +0530

    mac settings update

--- nixos-config/darwin-configuration.nix
+++ nixos-config/darwin-configuration.nix
@@ -28,6 +28,16 @@
       autohide = false;
       show-recents = false;
       minimize-to-application = true;
+      persistent-apps = [
+        "/Applications/Safari.app"
+        "/Applications/Brave Browser.app"
+        "/Applications/WhatsApp.app"
+        "/System/Applications/Mail.app"
+        "/Applications/Microsoft Word.app"
+        "/System/Applications/Notes.app"
+        "/Users/ishaan/Applications/Home Manager Apps/kitty.app"
+        "/System/Applications/App Store.app"
+      ];
     };
     finder = {
       AppleShowAllExtensions = true;
@@ -67,15 +77,29 @@
       autoUpdate = true;
       cleanup = "zap";
     };
+
+    # Mac App Store apps (requires you to be signed in to the App Store)
+    masApps = {
+      "Microsoft Excel" = 462058435;
+      "Microsoft Word" = 462054704;
+      "Microsoft PowerPoint" = 462062816;
+
+      "Dropover" = 1355679052;
+      "WhatsApp Messenger" = 310633997;
+    };
+
+    # GUI Apps
     casks = [
       "firefox"
       "brave-browser"
       "bitwarden"
       # "discord"
     ];
+
+    # CLI tools not in nixpkgs
     brews = [
-      # CLI tools not in nixpkgs
     ];
+
   };
 
   # Set shell
