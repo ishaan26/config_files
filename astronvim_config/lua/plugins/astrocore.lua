1ee6e1751dd54d79b8a8f1a239751fe08460a6a0
Ishaan Goel <ishaangoel.99@gmail.com>
2026-03-14 19:05:50 +0530

    increase editor tapstop

--- astronvim_config/lua/plugins/astrocore.lua
+++ astronvim_config/lua/plugins/astrocore.lua
@@ -28,6 +28,7 @@ return {
         spell = false,     -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = true,       -- sets vim.opt.wrap
+        tabstop = 4  
       },
       g = {                -- vim.g.<key>
         -- configure global vim variables (vim.g)
