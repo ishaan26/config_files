ca26c35f529895bf1e668edc88de1eb9b85f7ecc
Ishaan Goel <ishaangoel.99@gmail.com>
2026-04-01 17:26:52 +0530

    switch to astronvim v6

--- astronvim_config/lua-old/plugins/lang_surql.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"dariuscorvus/tree-sitter-surrealdb.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	config = function()
-		-- setup step
-		require("tree-sitter-surrealdb").setup()
-	end,
-}
--- astronvim_config/lua/plugins/lang_rust.lua
+++ astronvim_config/lua/plugins/lang_rust.lua
@@ -1,6 +1,8 @@
 return {
 	{
 		"mrcjkb/rustaceanvim",
+		version = "^8", -- Recommended
+		lazy = false, -- This plugin is already lazy
 		opts = {
 			tools = {
 				hover_action = {
